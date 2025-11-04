ccec0c5956df541fbc8a765926de244234fe6d96
CHAMRTHI VENKATESH <venkateshchamarthi412@gmail.com>
2025-11-04 12:53:00 +0530

    variable added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const name = "Eagle";
 
   const addTask = (taskText) => {
     if (taskText.trim() === '') return;
